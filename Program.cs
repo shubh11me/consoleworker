07fc1105cde281812b3876fa94c5842e96ae693c
shubh11me <todkarshubham11@gmail.com>
2023-08-14 14:48:40 +0530

    a

--- Program.cs
+++ Program.cs
@@ -1,11 +1,75 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 
 namespace consoleworker
 {
     class Program
     {
+
+        static int fibrecursive(int no)
+        {
+            if (no <= 1)
+                return no;
+            return fibrecursive(no - 1) + fibrecursive(no - 2);
+        }
+
+        static void fibprint(int no)
+        {
+
+            for (int i = 0; i < no; i++)
+            {
+                if (i == 6)
+                {
+                    continue;
+                }
+
+                Console.WriteLine("index {0}: {1}", i==6?null:i, i == 6 ? null : fibrecursive(i));
+            }
+
+        }
+
         static void Main(string[] args)
         {
+
+            string saaa = "I am <man> My name is <name> my fav color is <color>";
+           var arr= saaa.Split(' ');
+            //Console.WriteLine(new JsonResult(arr).Value );
+            foreach(var m in arr)
+            {
+                Console.WriteLine(m);
+            }
+            Console.ReadKey();
+
+            var name = "Shubham";
+            var sname = "Todakr";
+
+            Console.WriteLine("My name is {0} {1}", name,sname);
+            Console.ReadKey();
+
+
+            fibprint(10);
+            Console.ReadKey();
+            int[] absaaaaa= new int[5] { 0,0,0,0,0};
+            int count = 0;
+            foreach(var i in absaaaaa)
+            {
+                //Console.WriteLine(i);
+                Console.Write($"Enter {count+1} number:-");
+                absaaaaa[count] = Convert.ToInt32(Console.ReadLine());
+                count++;
+            }
+            foreach(var innn in absaaaaa)
+            {
+                Console.WriteLine(innn);
+            }
+            //done
+
+
+
+
+
+
+
             //Console.WriteLine("Myyyyy");
 
 
